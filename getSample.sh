f8594ae7823dc37c80c095d58d15f5c5d4b6c10d
John Heinnickel <john.heinnickel@thermofisher.com>
2026-05-06 19:14:23 +0000

    Common counter data collection and strucured extraction utiltities

---
+++ getSample.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -e
+
+AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_REGION
+
+script_dir="$( /usr/bin/dirname "${0}" )"
+bucket_name="${1}"
+path_prefix="${2}"
+
+epoch_seconds="$( /bin/date +%s )"
+day_num="$(( epoch_seconds / 86400 ))"
+day_second="$(( epoch_seconds % day_num ))"
+tos3="$( /bin/mktemp )"
+/bin/rm "${tos3}"
+/usr/bin/mkfifo "${tos3}"
+
+instance_id="$(cloud-init query instance-id)"
+instance_name="$(aws ec2 describe-instances --instance-id "${instance_id}" | jq -r '.Reservations[0].Instances[0].Tags[] | select(.Key == "Name") | .Value')"
+
+/bin/cat "${tos3}" | aws s3 cp - "s3://${bucket_name}/${path_prefix}/${instance_name}/${day_num}_${day_second}/idx.log" &
+echo "s3://${bucket_name}/${path_prefix}/${instance_name}/${day_num}_${day_second}/idx.log"
+"${script_dir}/keyhole" -index mongodb://127.0.0.1:27017/Collections 2>&1 | tee "${tos3}"
+echo "Fin"
---
+++ rawToCsv.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+
+
+sub calculate_use {
+	my $targetFile = shift;
+	my $outputFile = shift;
+	open(INPUT, $targetFile);
+	open(RAWRECORDS, ">${outputFile}");
+	print RAWRECORDS "VariomeName|IndexName|UseCount|Since\n";
+	my %collections = {};
+	my %indices = {};
+
+	my @readStates = ['expect_collection', 'expect_index', 'expect_index_or_collection', 'expect_opcount'];
+	my $readState = 'expect_collection';
+	my $trackedVariome = {};
+	my $lineCount = 0;
+	my $inputLine = '';
+
+	while($inputLine = <INPUT>) {
+		chomp $inputLine;
+		# print("## ${inputLine} ##\n");
+		$lineCount = $lineCount + 1;
+		if ($readState eq 'expect_collection') {
+			if ($inputLine =~ /^(Variome.*):$/) {
+				$collectionName = $1;
+				$readState = 'expect_index';
+			} elsif (($inputLine =~ /simagix\/keyhole/) or ($inputLine =~ /I GetIndexes ends/)) { 
+				# No-op for verbosity preamble/postinfo
+			} else {
+				print "Compilation error!  Expected Variome line, but read <${inputLine}> at <${lineCount}>\n";
+				exit -1;
+			}
+		} elsif ($readState eq 'expect_index_or_collection') {
+			if ($inputLine =~ /^(Variome.*):$/) {
+				$collectionName = $1;
+				$indexName = '';
+				$readState = 'expect_index';
+			} elsif ($inputLine =~ /^{.*}$/) {
+				$indexName = $inputLine;
+				$readState = 'expect_opcount';
+			} elsif ($inputLine =~ /is larger than the max int32/) {
+				# No-op for verbosity
+			} else {
+				print "Compilation error!  Expected Variome or index line, but read <${inputLine}> at <${lineCount}>\n";
+				exit -1;
+			}
+		} elsif ($readState eq 'expect_index') {
+			if ($inputLine =~ /^{.*}$/) {
+				$indexName = $inputLine;
+				$readState = 'expect_opcount';
+			} else {
+				print "Compilation error!  Expected index line, but read <${inputLine}> at <${lineCount}>\n";
+				exit -1;
+			}
+		} elsif ($readState eq 'expect_opcount') {
+			if ($inputLine =~ /ops: ([0-9]+), since: (.* UTC)/) {
+				my $opCount = $1;
+	   			my $since = $2;
+				print RAWRECORDS "${collectionName}|${indexName}|${opCount}|${since}\n";
+				$indexName = '';
+				$readState = 'expect_index_or_collection';
+			} else {
+				print "Compilation error!  Expected op count line, but read <${inputLine}> at <${lineCount}>\n";
+				exit -1;
+			}
+		} else {
+			print "Unknown read state?  <${readState}> at <${lineCount}>\n";
+			exit -1;
+		}
+	}
+
+	close RAWRECORDS;
+	close INPUT;
+}
+
+my $fileArg = $ARGV[0];
+my $outputArg = $ARGV[1];
+calculate_use($fileArg, $outputArg);
